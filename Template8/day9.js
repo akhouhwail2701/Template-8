e2cfdb8e2c0d3c2c0932b1570a860c2e5da12023
akhouhwail2701 <102318140+akhouhwail2701@users.noreply.github.com>
2023-09-18 08:26:33 -0700

    Add files via upload

---
+++ Template8/day9.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").onclick = function () {
+    if (document.getElementById("menu").style.visibility == "hidden") {
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    else {
+        document.getElementById("menu").style.visibility = "hidden"
+    }
+
+}
